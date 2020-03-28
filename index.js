f9d3217c555c4a3d9a5c429a3ae18ecbd9022fb2
George Rossakis <george@MacBook-Pro-3.local>
2020-03-28 12:08:25 -0400

    mobilefixes

--- index.js
+++ index.js
@@ -50,14 +50,15 @@ let rand = (min,max)=>parseInt((Math.random()*(max+1))+min);
 
 mSvg.on('touchmove',(d,i,n)=>{
 
-    let mouseX = d3.touches(n[i])[0][0];
-    console.log(mouseX)
-    let mouseY = d3.touches(n[i])[0][1];
-    let winWidth = window.innerWidth;
-    let winHeight = window.innerHeight;
+    let touchX = d3.touches(n[i])[0][0];
+    // console.log(mouseX)
+    let touchY = d3.touches(n[i])[0][1];
+    // console.log(mouseY)
+    // let winWidth = window.innerWidth;
+    // let winHeight = window.innerHeight;
 
-    let x = parseFloat(((mouseX/winWidth)*100).toFixed(0));
-    let y = parseFloat(((mouseY/winHeight)*100).toFixed(0));
+    // let x = parseFloat(((mouseX/winWidth)*100).toFixed(0));
+    // let y = parseFloat(((mouseY/winHeight)*100).toFixed(0));
 
     // mSvg.style('background-color', ()=>'hsl(173, '+x+'%, '+y/2+'%)');
 
@@ -65,23 +66,23 @@ mSvg.on('touchmove',(d,i,n)=>{
     // if(mSvg._groups[0][0].children.length < 150){
 
         mSvg.append('circle')
-            .attr('cx',x)
-            .attr('cy',y)
+            .attr('cx',touchX)
+            .attr('cy',touchY)
             .attr('r',3)
             .style('fill', 'none')
             .style('stroke-width', 0.1)
-            .style('stroke', ()=>'hsl('+((359*x)/100).toFixed(0)+', '+x+'%, 50%)')
+            .style('stroke', ()=>'hsl('+((359*touchX)/100).toFixed(0)+', 100%, 50%)')
             .transition()
-            .duration(2000)
-            .attr("r", 50)
-            // .style("opacity", 0)
+            .duration(1000)
+            .attr("r", 30)
+            .style("opacity", 0)
             .style('stroke-width', 1)
             // .attr('cx',(d,i,n)=>d3.select(n[i]).attr('cy')+0.001)
             .attr('cx',(d,i,n)=>{
-                return (Math.random()*100)>50? x+(Math.random()*100) : x-(Math.random()*100)
+                return (Math.random()*100)>50? touchX+(Math.random()*100) : touchX-(Math.random()*100)
             })
             .attr('cy',(d,i,n)=>{
-                return (Math.random()*100)>50? y+(Math.random()*100) : y-(Math.random()*100)
+                return (Math.random()*100)>50? touchY+(Math.random()*100) : touchY-(Math.random()*100)
             })
             .remove()
     // }
