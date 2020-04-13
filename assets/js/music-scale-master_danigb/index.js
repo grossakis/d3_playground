6459d1b59560ba4c193396622f1e941567092925
George Rossakis <george@MacBook-Pro-3.local>
2020-04-12 20:07:42 -0400

    step sequencer added

--- assets/js/music-scale-master_danigb/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var scale = require('./scale')
-scale.dictionary = require('./dictionary')
-scale.fromName = require('./fromName')
-scale.names = require('./names')
-
-module.exports = scale
